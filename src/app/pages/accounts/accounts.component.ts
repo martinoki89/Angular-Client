9d29ceecf41b1309332a1756da78437ee3c8de65
Martin Occhi <martinoki@hotmail.com>
2024-10-22 17:31:23 -0300

    feat: change styles and input autocomplete

--- src/app/pages/accounts/accounts.component.ts
+++ src/app/pages/accounts/accounts.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -12,6 +12,9 @@ import { MatInputModule } from '@angular/material/input';
 import { AccountsService } from '../../services/accounts.service';
 import { MatListModule } from '@angular/material/list';
 import { Router, RouterModule } from '@angular/router';
+import { debounceTime } from 'rxjs';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatOptionSelectionChange } from '@angular/material/core';
 
 @Component({
   selector: 'app-accounts',
@@ -25,30 +28,50 @@ import { Router, RouterModule } from '@angular/router';
     MatInputModule,
     MatButtonModule,
     MatListModule,
+    MatAutocompleteModule,
     RouterModule,
   ],
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.scss',
 })
-export class AccountsComponent {
+export class AccountsComponent implements OnInit {
   accountsFormGroup = new FormGroup({
-    name: new FormControl(),
+    account: new FormControl(),
   });
   accounts: any[] = [];
+  selectedAccountId?: string;
+
+  ngOnInit(): void {
+    this.accountsFormGroup
+      .get('account')
+      ?.valueChanges.pipe(debounceTime(1000))
+      .subscribe((value) => {
+        if (value.length >= 3) {
+          this.searchAccount(value);
+        }
+      });
+  }
 
   constructor(
     private readonly accountsService: AccountsService,
     private readonly router: Router
   ) {}
 
-  searchAccount() {
-    const { name } = this.accountsFormGroup.controls;
-    this.accountsService.getAccount(name.value).subscribe((response: any) => {
+  searchAccount(account: string) {
+    this.accountsService.getAccount(account).subscribe((response: any) => {
       this.accounts = response;
     });
   }
 
-  goToReport(account: any) {
-    this.router.navigate([`reports/${account.ID}`]);
+  displayFn(account: any): string {
+    return account?.Name || '';
+  }
+
+  goToReport() {
+    this.router.navigate([`reports/${this.selectedAccountId}`]);
+  }
+
+  selectedData(data: MatOptionSelectionChange) {
+    this.selectedAccountId = data.source.value.ID;
   }
 }
